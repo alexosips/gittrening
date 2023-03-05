d91c4c66dda52ce5d3e77c1c45974733c91706df
alexosips <alexanderlive15092003@gmail.com>
2023-03-05 23:22:18 +0200

    Зробив вивод  красивішим

--- papka/main.py
+++ papka/main.py
@@ -9,16 +9,16 @@ class Start:
         for i in range(0, 10):
             self.list.append(random.randint(1, 40))
     def print(self):
-        print(self.list)
+        print(f"Вивод списка: {self.list}")
 
     def arrange(self):
        self.list.sort()
-       print(self.list)
+       print(f"Від меншого до більшого: {self.list}")
 
     def revers(self):
        self.list.reverse ()
-       print(self.list)
-        #написати від більшого до меншого
+       print(f"Від більшого до меншого: {self.list}")
+
 
 start = Start()
 start.print()
