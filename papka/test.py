ae3e76b36869f914449f9068f822398a42178c7b
yarich0k <shyshyaroslav754@gmail.com>
2023-03-08 23:56:45 +0200

    виконав практикум

--- papka/test.py
+++ papka/test.py
@@ -1,9 +1,14 @@
-import main
-
-class Testpy:
-    #вивести список з файлу main.py
-    def print(self):
-        print(main.start.list)
-
-testpy = Testpy()
-testpy.print()
+import math
+global y
+x = float(input("Введіть значеннь x ="))
+if x < -2:
+    y = 1
+elif x >= -2 and x < 0:
+     y = -(1/2) * x - 0
+elif x >= 0 and x > 2:
+    y =math.sqrt(4+x ** 2)
+elif x <= 2 and x >= 0:
+    y = math.sqrt(x-4 ** 2)
+elif x >= 4 and y==0:
+    y = (2/3)*6
+print("X = {0: .2f}  Y ={1: .2f}".format(x, y))
