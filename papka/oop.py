8ab291e394b095ba77a54b6ecdb6dd54c50c1a95
alexosips <alexanderlive15092003@gmail.com>
2023-03-12 01:09:54 +0200

    kp3

--- papka/oop.py
+++ papka/oop.py
@@ -4,7 +4,7 @@
 Повернути периметр”, “ Повернути тип” (квадрат/прямокутник). Конструктор
 класу повинен приймати розміри сторін А та В. Якщо сторони не задані при
 створенні класу, то повинен створюватись квадрат зі сторонами А = 1 и В = 1."""
-
+"""
 class Car:
 
     def __init__(self, name, volume, color):
@@ -35,3 +35,36 @@ ycar.plus()
 
 er = Error()
 er.error()
+
+"""
+class Kp3:
+    def __init__(self):
+        string1 = "Я люблю Україну Слава України Слава"
+        string2 = "Київ столиця України Cлава Слава"
+        self.words1 = string1.split(' ')
+        self.words2 = string2.split(' ')
+        print(self.words1)
+        print(self.words2)
+    def rozglat(self):
+        print("\nВаріант B\n")
+        for i in range(len(self.words1)):
+            if self.words1[i] in self.words2:
+                print(f"Слово '{self.words1[i]}' знайдено")
+            else:
+                print(f"Слово '{self.words1[i]}' не знайдено")
+    def norozglat(self):
+        list = []
+        print("\nВаріант А\n")
+        for i in range(len(self.words1)):
+            if self.words1[i] in self.words2:
+                list.append(self.words1[i])
+                if list.count(self.words1[i]) >1 :
+                    print(f"Слово '{self.words1[i]}' вже було")
+                else:
+                    print(f"Слово '{self.words1[i]}' знайдено")
+            else:
+                print(f"Слово '{self.words1[i]}' не знайдено")
+
+kp3 = Kp3()
+kp3.rozglat()
+kp3.norozglat()
