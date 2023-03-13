0d746f0b83df6f0d793a1b959ef454f7cab46623
alexosips <alexanderlive15092003@gmail.com>
2023-03-13 19:42:46 +0200

    exmale 50000

--- papka/testnikit.py
+++ papka/testnikit.py
@@ -1,30 +1,35 @@
 import random
+from datetime import datetime
+import time
 """ 1)Вивести будь-яку фразу 50 раз
     2)Вивести список через for (кожен елемент з нового рядка)
     3)Порівняти кожен елмент списка з числом 13(більше менше)
-    4)Вивести найбільше значення списка"""
+    4)Вивести найбільше значення списка
+    5)Порахуй скільки елментів 13 в списку"""
 
 list = []
-for i in range(0,50):
+
+for i in range(0,5000000):
     list.append(random.randint(1,100))
 print(f"Початковий список\n {list}")
 
+
 # 1
-for i in range(0,50):
-    print(f"{i} Putin xuilo")
+#for i in range(0,50):
+  # print(f"{i} Putin xuilo")
 
 # 2
-for i in range(len(list)):
-    print(f"Елемент : {i} -> {list[i]}")
+#for i in range(len(list)):
+    #print(f"Елемент : {i} -> {list[i]}")
 
 # 3
-for i in range(len(list)):
-    if list[i] > 13:
-        print(f"Element: {list[i]} більше 13")
-    elif list[i] < 13:
-        print(f"Element: {list[i]} менше 13")
-    else:
-        print(f"Element: {list[i]} рівно 13")
+#for i in range(len(list)):
+    #if list[i] > 13:
+     #   print(f"Element: {list[i]} більше 13")
+    #elif list[i] < 13:
+     #   print(f"Element: {list[i]} менше 13")
+    #else:
+     #   print(f"Element: {list[i]} рівно 13")
 # 4
-print(max(list))
-print(min(list))
+#print(max(list))
+#print(min(list))
