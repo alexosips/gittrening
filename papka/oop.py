2db79623c2ae55fbaea3c34c7ecf8a7bbe7513b4
alexosips <alexanderlive15092003@gmail.com>
2023-03-15 19:48:46 +0200

    + 2 шмари

--- papka/oop.py
+++ papka/oop.py
@@ -26,10 +26,40 @@ class Pramokutnik:
             print("квадрат")
         else:
             print("прямокутник")
-a = Pramokutnik(5,5)
-a.reversa()
-a.reversb()
-a.plosh()
-a.perimeter()
-a.type()
+#a = Pramokutnik(5,5)
+#a.reversa(),a.reversb(),a.plosh(),a.perimeter(),a.type()
+class Shluxa:
+    def __init__(self,dolorov_v_chas,rozmer_sisok,name):
+        self.dolorov_v_chas = dolorov_v_chas
+        self.rozmer_sisok = rozmer_sisok
+        self.name = name
 
+
+    def info(self):
+        print(f"Шлюха на ім'я {self.name} має {self.rozmer_sisok} розмір сісічок та бере вона {self.dolorov_v_chas}"
+              f" доляров в годину")
+
+    def norm_nenorm(self,old):
+        if old >=30:
+            print("Та ну її нахуй")
+        else:print("Ще покатить возраст")
+
+class Sluxa2(Shluxa):
+    def __init__(self,dolorov_v_chas,rozmer_sisok,name,rost):
+        super().__init__(dolorov_v_chas,rozmer_sisok,name)
+        self.rost = rost
+    def info(self):
+        print(self.rost,self.dolorov_v_chas)
+
+    def norm_nenorm(self,rost,old):
+        super().norm_nenorm(old)
+        if rost >=170:
+            print("Та ну його нахуй шапала")
+        else:
+            print("Норм піде рост")
+
+shluxa = Shluxa(15,2,"Анжела")
+#shluxa.norm_nenorm(29)
+sluxa2 = Sluxa2(15,2,"Анжела",125)
+sluxa2.info()
+sluxa2.norm_nenorm(156,25)
