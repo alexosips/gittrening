f92fa8f51b5304a7e4e2219e7095cac8d11db6fc
alexosips <alexanderlive15092003@gmail.com>
2023-03-27 16:24:18 +0300

    crate files and write, and remove + comment

--- papka/oop.py
+++ papka/oop.py
@@ -1,23 +1,75 @@
-#english trening
-englishlist = []
-ukrlist = []
+# english trening
 class Hello:
     def __init__(self):
-        print("Вас зустрічає скрипт для вивчення слів з англйської мови")
-        if len(ukrlist) == 0 and len(englishlist) == 0:
+        print("\nВас зустрічає скрипт для вивчення слів з англйської мови")
+        self.englishlist = ''  # пусті стрінги які перетворяться в списки
+        self.ukrlist = ''
+        if len(self.printukr()) == 0 and len(self.printenglish()) == 0:  # Перевірка на наявність елементів у файлах
             print("У вас покищо немає жодного слова")
-            Hello.add(self)
+            Hello.add(self)  # У разі пустих файлів запускаєм функцію по додаванню слів
+            # Hello.print(self)
         else:
-            pass
+            number = input(  # Інакше запускаємо одну з вибраних дій
+                "1-> Показать всі слова англ\n"
+                "2-> Показать всі слова укр\n"
+                "3-> Додати нове слово\n"
+                "4-> Видалити всі слова\n"
+                "Введіть число: ")
+            if number == "1":
+                print(self.printenglish())  # Функція виводу інгліш слів
+                Hello()
+            elif number == "2":
+                print(self.ukrlist)  # Функція виводу укр слів
+                Hello()
+            elif number == "3":
+                self.add()  # Функція додавання слів
+            elif number == "4":
+                self.delateall()  # Функція видалення всіх слів
+                Hello()
+            else:
+                Hello()
+
     def add(self):
-        english = input("Введіть слово з англиської мови: ")
-        ukr = input("Введіть його переклад: ")
+        english = input("Введіть слово з англиської мови: ")  # Вводимо потрібні нам слова
+        ukr = input("Введіть його переклад: ")  # Та їх переклад
         if english != ukr:
-            englishlist.append(english)
-            ukrlist.append(ukr)
+            f = open('english.txt', 'a')  # Відкриваємо файл
+            f.write("\t")  # Робимо відступ
+            f.write(english)  # Вставляємо наше слово
+            f.close()  # Закриваємо файл
+            print("\nВиводим данні з файлу\n")
+            print(self.printenglish())
+            f2 = open('ukr.txt', 'a')  # Аналоігчні дії для перекладу
+            f2.write('\t')
+            f2.write(ukr)
+            f2.close()
+            print(self.printukr())
+
         else:
             print("Спробуйте ще раз")
-            Hello.add(self)
-    def indo(self):
-        pass
+            self.add()
+
+    def printenglish(self): # Функція виводу інгліш слів
+        file = open('english.txt', 'r') # Відкриваєм файл з аргументом 'a'
+        for line in file:
+            d = line.split() # Кожен рядок розбиваємо на окремі слова і виходить список
+            self.englishlist = d # Записуєм отриманий список в наш стрінг
+        file.close() # Закриваємо файл
+        return self.englishlist # Повертаємо список
+
+    def printukr(self): # Аналогічно попередньому
+        file = open('ukr.txt', 'r')
+        for line in file:
+            d = line.split()
+            self.ukrlist = d
+        file.close()
+        return self.ukrlist
+
+    def delateall(self):
+        f1 = open('english.txt', 'w')  # При аргементі 'w' всі данні файлу перезаписуються
+        f2 = open('ukr.txt', 'w')
+        f1.close()  # Закриваємо файли
+        f2.close()
+
+
 hello = Hello()
