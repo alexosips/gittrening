7c4f98cd8aef45f8cc2ea837bfc86732fc4f01ad
alexosips <alexanderlive15092003@gmail.com>
2023-04-05 14:13:21 +0300

    kp tochno

--- papka/test.py
+++ papka/test.py
@@ -1,8 +1,20 @@
-texte = "Cьоаані вранці була гаааа погода, а ввечері ні"
+texte = "Cьоаані вранці була гаааа гаааа погода, а ввечері ні"
 text = texte.lower()
-text = text.split()
-selected_words = [word for word in text if len(word) %2 == 1]
-max_vowels = max(selected_words, key=lambda word: sum(letter in 'аеиоуюя' for letter in word))
-selected_words_max_vowels = [word for word in selected_words if sum(letter in 'аеиоуюя' for letter in word) == sum(letter in 'аеиоуюя' for letter in max_vowels)]
-text.remove(selected_words_max_vowels[0])
+result_words = []
+for word in text.split():
+    if len(word) % 2 != 0:
+        result_words.append(word)
+print(result_words)
+max_vowel_count = 0
+
+list = []
+for word in result_words:
+    vowel_count = sum(letter in 'аеиоуюя' for letter in word)
+    if vowel_count > max_vowel_count:
+        max_vowel_count = vowel_count
+        list.append(word)
+
+print(list)
+for word in list:
+    text = text.replace(word, '')
 print(text)
