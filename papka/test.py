de1eef4781b0e8c54c87248930884eeb714e6589
alexosips <alexanderlive15092003@gmail.com>
2023-04-05 12:12:14 +0300

    готова кп

--- papka/test.py
+++ papka/test.py
@@ -1,26 +1,8 @@
-import re
-
-# Попросити користувача ввести текст в декілька рядків
-text = input("Введіть текст в декілька рядків:\n")
-
-# Розділити текст на окремі рядки
-lines = text.split('\n')
-
-# Визначити, які рядки є непарними
-odd_lines = [i for i in range(len(lines)) if i % 2 != 0]
-
-# Для кожного непарного рядка видалити слово, яке містить найбільшу кількість голосних {а, е, i, ї, о, и, y, я, є, ю}
-for i in odd_lines:
-    words = re.findall(r'\b\w+\b', lines[i])
-    max_vowels = 0
-    max_vowels_word = ''
-    for word in words:
-        vowel_count = len(re.findall(r'[аеiїоиyяєю]', word.lower()))
-        if vowel_count > max_vowels:
-            max_vowels = vowel_count
-            max_vowels_word = word
-    if max_vowels_word:
-        lines[i] = lines[i].replace(max_vowels_word, '')
-
-# Вивести оновлений текст
-print("\n".join(lines))
+texte = "Cьоаані вранці була гаааа погода, а ввечері ні"
+text = texte.lower()
+text = text.split()
+selected_words = [word for word in text if len(word) %2 == 1]
+max_vowels = max(selected_words, key=lambda word: sum(letter in 'аеиоуюя' for letter in word))
+selected_words_max_vowels = [word for word in selected_words if sum(letter in 'аеиоуюя' for letter in word) == sum(letter in 'аеиоуюя' for letter in max_vowels)]
+text.remove(selected_words_max_vowels[0])
+print(text)
