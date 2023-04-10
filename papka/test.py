1abce4c36bd104adfec36dc5084a826f2c629fb1
alexosips <alexanderlive15092003@gmail.com>
2023-04-10 23:09:34 +0300

    eroor

--- papka/test.py
+++ papka/test.py
@@ -24,18 +24,45 @@ for i in range(len(textlist)):
     neparno(textlist[i])
 print(f"З першого рядка: {result_words}\n Другого: {result_words2}\n"
           f"Третього: {result_words3}")
-"""
-max_vowel_count = 0
-
+result_wordses = [result_words,result_words2,result_words3]
 list = []
-for word in result_words:
-    vowel_count = sum(letter in 'аеиоуюя' for letter in word)
-    if vowel_count > max_vowel_count:
-        max_vowel_count = vowel_count
-        list.append(word)
+list2 = []
+list3 = []
+lse = [list,list2,list3]
+def suma(result):
+    max_vowel_count = 0
+    for word in result:
+        if result == result_words:
+            vowel_count = sum(letter in 'аеиоуюя' for letter in word)
+            if vowel_count > max_vowel_count:
+                max_vowel_count = vowel_count
+                list.append(word)
+        elif result == result_words2:
+            vowel_count = sum(letter in 'аеиоуюя' for letter in word)
+            if vowel_count > max_vowel_count:
+                max_vowel_count = vowel_count
+                list2.append(word)
+        elif result == result_words3:
+            vowel_count = sum(letter in 'аеиоуюя' for letter in word)
+            if vowel_count > max_vowel_count:
+                max_vowel_count = vowel_count
+                list3.append(word)
 
-print(list)
-for word in list:
-    text = text.replace(word, '')
-print(text)
-"""
+for i in range(len(result_wordses)):
+    suma(result_wordses[i])
+print(f"Слова з найбільшою кількістю голосних з \nпершого речення: {list}\n"
+      f"Другого: {list2}\n"
+      f"Третього: {list3}")
+def dellate(ls):
+    for word in ls:
+        if ls == list:
+            texte = texte.replace(word, '')
+        elif ls == list2:
+            texte2 = texte2.replace(word, '')
+        elif ls == list3:
+            texte3 = texte3.replace(word, '')
+
+
+for i in range(len(lse)):
+    dellate(lse[i])
+    print(f"{texte}\n{texte2}\n{texte3}")
